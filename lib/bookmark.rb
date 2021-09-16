2aa3a6c87039c7cb0c2033b58f2e48c2dfdd5bad
EMDevelop <edwardmartin.inbox@gmail.com>
2021-09-16 19:07:40 +0100

    safe against SQL injection

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -22,7 +22,7 @@ class Bookmark
     assign_database
     begin
       con = PG.connect :dbname => @env, :user => ENV['DBUSER'], :password => ENV['DBPASSWORD']
-      result = con.exec("INSERT INTO bookmarks (url, title) VALUES ('#{url}', '#{title}') RETURNING id, url, title;")
+      result = con.exec_params("INSERT INTO bookmarks (url, title) VALUES ($1,$2) RETURNING id, url, title;", [url,title])
       Bookmark.new(id: result[0]['id'], title:result[0]['title'], url:result[0]['url'])
     rescue PG::Error => e
       puts e.message 
